90a9469003821707e94bbccbcaf877a2c22c0313
Lucilite <gofafafa1@gmail.com>
2025-03-10 23:27:03 +0700

    Updated deployment with latest build

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://3nbdi5jfzf.execute-api.ap-southeast-1.amazonaws.com/Deploy',
-  envName: 'PRODUCTION'
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: '/api',  // This will be rewritten by the proxy to point to AWS
-  envName: 'DEVELOPMENT'
-};
